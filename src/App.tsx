743c167e215773b5aa56d0c0420ccab3e5de76ff
Gustavo <gupaluski@gmail.com>
2025-02-16 11:10:46 -0300

    :construction: wip: Create books page structure

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Homepage from './pages/Homepage/Homepage'
 import Navbar from './components/navbar/Navbar'
 import Footer from './components/Footer/Footer'
 import About from './pages/About/About'
+import Books from './pages/Books/Books'
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Homepage/>}/>
           <Route path='/about' element={<About/>}/>
+          <Route path='/books' element={<Books/>}/>
         </Routes>
         <Footer/>
       </BrowserRouter>
---
+++ src/pages/Books/Books.tsx
@@ -0,0 +1,11 @@
+import './Books.css';
+
+const Books = () => {
+    return(
+        <>
+            <h3>Books page</h3>
+        </>
+    );
+}
+
+export default Books;
