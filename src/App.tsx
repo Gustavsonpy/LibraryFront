7058a6ac4dc79d355b7974abab9fd7c72fcd46b5
Gustavo <gupaluski@gmail.com>
2025-02-16 10:32:47 -0300

    :construction: wip: Create about page structure

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css'
 import Homepage from './pages/Homepage/Homepage'
 import Navbar from './components/navbar/Navbar'
 import Footer from './components/Footer/Footer'
+import About from './pages/About/About'
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
         <Navbar/>
         <Routes>
           <Route path='/' element={<Homepage/>}/>
+          <Route path='/about' element={<About/>}/>
         </Routes>
         <Footer/>
       </BrowserRouter>
